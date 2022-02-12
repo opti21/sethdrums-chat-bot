322772e0f74a0855fcfe631465e5ad1352c3bf83
opti21 <40129778+opti21@users.noreply.github.com>
2022-02-12 15:31:58 -0600

    updated redis connection

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,8 @@ const twitch = new tmi.Client({
 const prisma = new PrismaClient();
 
 const redis = new Redis({
+  port: 6379,
+  host: process.env.REDIS_HOST ? process.env.REDIS_HOST : "",
   password: process.env.REDIS_PASS,
 });
 
