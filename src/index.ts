e76b22778620b57364f40eef763d29840a919be5
opti21 <40129778+opti21@users.noreply.github.com>
2025-05-15 15:00:36 -0500

    refactor: remove WebSocket implementation and clean up Pusher configuration

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,10 @@
 import { Hono } from "hono";
-import { createBunWebSocket } from "hono/bun";
-import type { ServerWebSocket } from "bun";
 import { twitch } from "chatbot";
 import Pusher from "pusher";
 
 const app = new Hono();
 
-const { upgradeWebSocket, websocket } = createBunWebSocket<ServerWebSocket>();
-
-const rooms = new Map<string, Set<ServerWebSocket>>();
-const topic = "sethdrums-queue";
-
 app.get("/", (c) => c.text("PepegaBot is running!"));
-
-app.get(
-  "/ws",
-  upgradeWebSocket((c) => {
-    return {
-      onMessage(event, ws) {
-        const rawWs = ws.raw as ServerWebSocket;
-        rawWs.subscribe("pepega");
-        console.log(
-          `WebSocket server opened and subscribed to topic '${topic}'`
-        );
-        ws.send("Hello from server!");
-      },
-      onClose: (_, ws) => {
-        const rawWs = ws.raw as ServerWebSocket;
-        rawWs.unsubscribe(topic);
-        console.log(
-          `WebSocket server closed and unsubscribed from topic '${topic}'`
-        );
-      },
-    };
-  })
-);
 
 twitch.connect().catch(console.error);
 
@@ -48,12 +18,10 @@ if (
 }
 
 export const pusher = new Pusher({
-  appId: process.env.PUSHER_APP_ID,
-  host: process.env.PUSHER_HOST!,
-  port: process.env.PUSHER_PORT!,
-  key: process.env.PUSHER_KEY,
-  secret: process.env.PUSHER_SECRET,
-  useTLS: true,
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
 });
 
 const port = process.env.PORT || 3000;
@@ -62,5 +30,4 @@ console.log(`Server is running on http://localhost:${port}`);
 Bun.serve({
   fetch: app.fetch,
   port: port,
-  websocket: websocket,
 });
